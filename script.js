ef237379c9828c9d231302af3ab697f49926e91e
Vanessa Baias <baias.vanessa1@gmail.com>
2022-02-23 15:28:16 -0800

    finished rockPaperScissors

--- script.js
+++ script.js
@@ -3,3 +3,34 @@ function computerPlay() {
   let outcome = ["rock", "paper", "scissors"];
   return outcome[randomNumber];
 }
+
+function rockPaperScissors(
+  playerSelection,
+  computerSelection = computerPlay()
+) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "draw";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return " you lose";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "you lose";
+  } else if (playerSelection === " scissors" && computerSelection === "paper") {
+    return "you win";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "you win";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "you lose";
+  } else {
+    return "try again";
+  }
+}
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let userSelection = prompt("rock, paper or scissors?");
+    console.log(rockPaperScissors(userSelection));
+  }
+}
